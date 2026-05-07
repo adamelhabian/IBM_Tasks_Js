b06288d6b44a4f4af0b5c5e61d5222f44aa27959
Adam Elhabian <elhabianadam16@gmail.com>
2026-05-07 11:35:08 +0300

    Intial TodoList

---
+++ TodoList/todo_list.js
@@ -0,0 +1,43 @@
+let taskInput = document.getElementById("taskInput");
+
+const addTaskBtn = document.getElementById("addTaskBtn");
+const taskList = document.getElementById("taskList");
+const clearCompletedBtn = document.getElementById("clearCompletedBtn");
+
+let tasks = [];
+
+function addTask() {
+  let taskText = taskInput.value.trim();
+
+  if (taskText !== "") {
+    tasks.push({ text: taskText, completed: false });
+    taskInput.value = ""; // clearing the input for next task entry
+    displayTasks();
+  }
+}
+
+function displayTasks() {
+  taskList.innerHTML = "";
+
+  tasks.forEach((task, index) => {
+    let li = document.createElement("li");
+    li.innerHTML = `<input type="checkbox" id="task-${index}" ${task.completed ? "checked" : ""}> ${task.text}`;
+    li.querySelector("input").addEventListener("change", () =>
+      toggleTask(index),
+    );
+    taskList.appendChild(li);
+  });
+}
+
+function toggleTask(index) {
+  tasks[index].completed = !tasks[index].completed;
+  displayTasks();
+}
+function clearCompletedTasks() {
+  tasks = tasks.filter((task) => !task.completed);
+  displayTasks();
+}
+addTaskBtn.addEventListener("click", addTask);
+
+clearCompletedBtn.addEventListener("click", clearCompletedTasks);
+displayTasks();
